f19377a4d1efd8dd88cf7b7630b2901ca57e4593
kirill-stupakov <kirill.stupakov.0@gmail.com>
2021-10-30 16:55:24 +0300

    Fix: Styling

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+const cors = require("cors");
 const { getStats, updateMessage } = require("./db");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
