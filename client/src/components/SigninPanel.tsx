7c06d87a059c52514e1602c4c2218e1323ab752b
kirill-stupakov <kirill.stupakov.0@gmail.com>
2021-11-02 17:26:59 +0300

    Refactor: Dedicated functions for signin

--- client/src/components/SigninPanel.tsx
+++ client/src/components/SigninPanel.tsx
@@ -3,13 +3,17 @@ import GoogleLogin from "react-google-login";
 import FacebookLogin from "react-facebook-login";
 // @ts-ignore
 import VkLogin from "react-vkontakte-login";
-// import VkLogin from "react-vk-login-button";
 
 interface Props {
   setUserName: (s: string) => void;
 }
 
 const SigninPanel: React.FC<Props> = ({ setUserName }) => {
+  const googleAuth = (res: any) =>
+    res.profileObj.name && setUserName(res.profileObj.name);
+  const facebookAuth = (res: any) => res.name && setUserName(res.name);
+  const VkAuth = (res: any) => console.log(res);
+
   return (
     <>
       <h3>Please, sign in to send messages</h3>
@@ -24,19 +28,19 @@ const SigninPanel: React.FC<Props> = ({ setUserName }) => {
             Google
           </button>
         )}
-        onSuccess={(res: any) => setUserName(res.profileObj.name)}
+        onSuccess={googleAuth}
         cookiePolicy="single_host_origin"
         icon={false}
       />
       <FacebookLogin
         appId="313394383566103"
-        callback={(res: any) => setUserName(res.name)}
+        callback={facebookAuth}
         cssClass="btn btn-primary px-4 rounded m-3 shadow-sm"
         textButton="Facebook"
       />
       <VkLogin
         clientId="7990349"
-        callback={(res: any) => console.log(res)}
+        callback={VkAuth}
         render={(renderProps: any) => (
           <button
             onClick={renderProps.onClick}
